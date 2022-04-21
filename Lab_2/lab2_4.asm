9ca7525177dd7bf22c0a8ea7db32e1250e0e1f82
rivrivboom <102767320+rivrivboom@users.noreply.github.com>
2022-04-21 10:35:09 -0700

    finale lab2_4
    rivera adrian

--- Lab_2/lab2_4.asm
+++ Lab_2/lab2_4.asm
@@ -1,6 +1,6 @@
 .data
-B : .word 0 1 2 4 8 16
-A: .word 0 0 0 0 0
+B : .word 0, 1, 2, 4, 8, 16
+A: .word 0, 0, 0, 0, 0, 0
 size: .word 5
 
 .text
@@ -11,32 +11,48 @@ size: .word 5
  	
  		
  	
- 	la a0, A
- 	la a1, B
+ 	la a1, A
+ 	la a2, B
  	li, a3, 0
- 	li, s8, 0
+ 	li, a4, 0
  	li, a5, 5
  	li, t1, 0
+ 	li t4,1
+ 	li t5, 2
 	 	
  	
- 		add t0, s8, s8
+ 		add t0, a4, a4
  	loop1:	
- 		slti t1, a3, 5
- 		beqz t1, loop2
+ 		slti t1, a3, 6
+ 		beqz t1, step2
  		slli, t2, a3, 2
- 		add s10, a0, t2
+ 		add s10, a1, t2
  		lw s1,0(s10)
- 		add s11, a1, t2
+ 		add s11, a2, t2
  		lw s2, 0(s11)
  		mv s0, s2
- 		add s2 ,s1, s2
- 		sw s11, 0(a0)
- 		mv s0,s
+ 		sub s2,s2,t4
+ 		add s1 ,s1, s2
+ 		sw s1, 0(s10)
  		addi a3, a3, 1
- 		#sub t6,t6,t6
- 		jal s8, loop1 		
- 	loop2: 
  		
+ 		jal a4, loop1 		
+ 	
+ 	step2: sub a3,a3,t4
+ 		j loop2
+ 	
+ 	loop2:  beqz a3, end
+ 		slli, t2, a3, 2
+ 		add s10, a1, t2
+ 		lw s1,0(s10)
+ 		add s11, a2, t2
+ 		lw s2, 0(s11)
+ 		mv s0, s2
+ 		add s1 ,s1, s2
+ 		mul s1, s1, t5
+ 		sw s1, 0(s10)
+ 		sub a3, a3, t4
+ 		j loop2
  		
  	end:
  		li a7,10
