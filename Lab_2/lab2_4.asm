fabca20ab46c4fa3d924545250583d857bb0a4b2
rivrivboom <102767320+rivrivboom@users.noreply.github.com>
2022-04-20 22:16:07 -0700

    update lab2_4

--- Lab_2/lab2_4.asm
+++ Lab_2/lab2_4.asm
@@ -27,10 +27,11 @@ size: .word 5
  		add s10, a0, t2
  		lw s1,0(s10)
  		add s11, a1, t2
- 		lw s2, 0(a1)
- 		add s1 ,s1, s11
+ 		lw s2, 0(s11)
+ 		mv s0, s2
+ 		add s2 ,s1, s2
  		sw s11, 0(a0)
- 		
+ 		mv s0,s
  		addi a3, a3, 1
  		#sub t6,t6,t6
  		jal s8, loop1 		
