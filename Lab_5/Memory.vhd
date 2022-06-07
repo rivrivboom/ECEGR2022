4fdc452c7915d7de0308df414e90206dd2f9daa6
rivrivboom <102767320+rivrivboom@users.noreply.github.com>
2022-06-06 19:05:34 -0700

    changes for lab6

--- Lab_5/Memory.vhd
+++ Lab_5/Memory.vhd
@@ -86,15 +86,25 @@ architecture remember of Registers is
 	
 begin
 
-	write_In <= "00000001" when WriteReg = "01010" and WriteCmd = '1' else
-		"00000010" when WriteReg = "01011" and WriteCmd = '1' else
-		"00000100" when WriteReg = "01100" and WriteCmd = '1' else
-		"00001000" when WriteReg = "01101" and WriteCmd = '1' else
-		"00010000" when WriteReg = "01110" and WriteCmd = '1' else
-		"00100000" when WriteReg = "01111" and WriteCmd = '1' else
-		"01000000" when WriteReg = "10000" and WriteCmd = '1' else
-		"10000000" when WriteReg = "10001" and WriteCmd = '1' else
-		"00000000";
+	write_In <= (1 => '1', OTHERS => '0') WHEN WriteReg = "00001" AND WriteCmd = '1' ELSE
+			(2 => '1', OTHERS => '0') WHEN WriteReg = "00010" AND WriteCmd = '1' ELSE
+			(3 => '1', OTHERS => '0') WHEN WriteReg = "00011" AND WriteCmd = '1' ELSE
+			(4 => '1', OTHERS => '0') WHEN WriteReg = "00100" AND WriteCmd = '1' ELSE
+			(5 => '1', OTHERS => '0') WHEN WriteReg = "00101" AND WriteCmd = '1' ELSE
+			(6 => '1', OTHERS => '0') WHEN WriteReg = "00110" AND WriteCmd = '1' ELSE
+			(7 => '1', OTHERS => '0') WHEN WriteReg = "00111" AND WriteCmd = '1' ELSE
+			(8 => '1', OTHERS => '0') WHEN WriteReg = "01000" AND WriteCmd = '1' ELSE
+			(9 => '1', OTHERS => '0') WHEN WriteReg = "01001" AND WriteCmd = '1' ELSE
+		    (10 => '1', OTHERS => '0') WHEN WriteReg ="01010" AND WriteCmd = '1' ELSE
+		    (11 => '1', OTHERS => '0') WHEN WriteReg ="01011" AND WriteCmd = '1' ELSE
+	  	    (12 => '1', OTHERS => '0') WHEN WriteReg ="01100" AND WriteCmd = '1' ELSE
+		    (13 => '1', OTHERS => '0') WHEN WriteReg ="01101" AND WriteCmd = '1' ELSE
+		    (14 => '1', OTHERS => '0') WHEN WriteReg = "01110" AND WriteCmd = '1' ELSE
+		    (15 => '1', OTHERS => '0') WHEN WriteReg ="01111" AND WriteCmd = '1' ELSE
+		    (16 => '1', OTHERS => '0') WHEN WriteReg ="10000" AND WriteCmd = '1' ELSE
+		(17 => '1', OTHERS => '0') WHEN WriteReg ="10001" AND WriteCmd = '1' ELSE
+		(18 => '1', OTHERS => '0') WHEN WriteReg ="10010" AND WriteCmd = '1' ELSE
+		(others => '0');
 
  	RegOrg: for i in 7 downto 0 generate
 		Ai: register32 port map (WriteData, '0', '1', '1', write_In(i), '0', '0',  regData(i));
@@ -102,26 +112,72 @@ begin
 	
 	
 	with ReadReg1 select
-		ReadData1 <= regData(0) when "01010",
-			regData(1) when "01011",
-			regData(2) when "01100",
-			regData(3) when "01101",
-			regData(4) when "01110",
-			regData(5) when "01111",
-			regData(6) when "10000",
-			regData(7) when "10001",
+		ReadData1 <= regData(1) WHEN "00001",
+				regData(2) WHEN "00010",
+				regData(3) WHEN "00011",
+				regData(4) WHEN "00100",
+				regData(5) WHEN "00101",
+				regData(6) WHEN "00110",
+				regData(7) WHEN "00111",
+				regData(8) WHEN "01000",
+				regData(9) WHEN "01001",
+				regData(10) WHEN "01010",
+				regData(11) WHEN "01011",
+				regData(12) WHEN "01100",
+				regData(13) WHEN "01101",
+				regData(14) WHEN "01110",
+				regData(15) WHEN "01111",
+				regData(16) WHEN "10000",
+				regData(17) WHEN "10001",
+				regData(18) WHEN "10010",
+				regData(19) WHEN "10011",
+				regData(20) WHEN "10100",
+				regData(21) WHEN "10101",
+				regData(22) WHEN "10110",
+				regData(23) WHEN "10111",
+				regData(24) WHEN "11000",
+				regData(25) WHEN "11001",
+				regData(26) WHEN "11010",
+				regData(27) WHEN "11011",
+				regData(28) WHEN "11100",
+				regData(29) WHEN "11101",
+				regData(30) WHEN "11110",
+				regData(31) WHEN "11111",
 			(others => '0') when "00000",
 			(others => 'Z') when others;
 
 	with ReadReg2 select
-		ReadData2 <= regData(0) when "01010",
-			regData(1) when "01011",
-			regData(2) when "01100",
-			regData(3) when "01101",
-			regData(4) when "01110",
-			regData(5) when "01111",
-			regData(6) when "10000",
-			regData(7) when "10001",
+		ReadData2 <= regData(1) WHEN "00001",
+				regData(2) WHEN "00010",
+				regData(3) WHEN "00011",
+				regData(4) WHEN "00100",
+				regData(5) WHEN "00101",
+				regData(6) WHEN "00110",
+				regData(7) WHEN "00111",
+				regData(8) WHEN "01000",
+				regData(9) WHEN "01001",
+				regData(10) WHEN "01010",
+				regData(11) WHEN "01011",
+				regData(12) WHEN "01100",
+				regData(13) WHEN "01101",
+				regData(14) WHEN "01110",
+				regData(15) WHEN "01111",
+				regData(16) WHEN "10000",
+				regData(17) WHEN "10001",
+				regData(18) WHEN "10010",
+				regData(19) WHEN "10011",
+				regData(20) WHEN "10100",
+				regData(21) WHEN "10101",
+				regData(22) WHEN "10110",
+				regData(23) WHEN "10111",
+				regData(24) WHEN "11000",
+				regData(25) WHEN "11001",
+				regData(26) WHEN "11010",
+				regData(27) WHEN "11011",
+				regData(28) WHEN "11100",
+				regData(29) WHEN "11101",
+				regData(30) WHEN "11110",
+				regData(31) WHEN "11111",
 			(others => '0') when "00000",
 			(others => 'Z') when others;
 
